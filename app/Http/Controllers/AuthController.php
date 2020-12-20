e47748c2d4d297812a483e426b38207107a74261
Fernando Alfonso Zhunio <fzhunio91@hotmail.com>
2020-12-20 11:52:32 -0500

    Primer commit con authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+
+class AuthController extends Controller
+{
+    // public function __construct(){
+    //  $this->middleware('api');
+    // }
+
+
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+            'remember_me' => 'boolean'
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if (!auth()->attempt($credentials))
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+
+        $user = $request->user();
+        $tokenResult = $user->createToken('Personal Access Token');
+
+        $token = $tokenResult->token;
+        if ($request->remember_me)
+            $token->expires_at = Carbon::now()->addWeeks(1);
+        $token->save();
+        $role = auth()->user()->getRoleNames();
+        $user = auth()->user();
+
+        return response()->json([
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'role'=> $role,
+            'user'=>$user,
+            'expires_at' => Carbon::parse($token->expires_at)->toDateTimeString()
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string'
+        ]);
+
+        $userCurrent = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $userCurrent->assignRole('usuario');
+
+        return response()->json([
+            'message' => 'Successfully created user!'
+        ], 201);
+    }
+
+       /**
+     * Cierre de sesión (anular el token)
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
+
+    /**
+     * Obtener el objeto User como json
+     */
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
+
+}
---
+++ database/seeds/RolesAndPermission.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Permisos
+        User::create([
+            'name' => 'Fernando Zhunio Reyes',
+            'email' => 'fzhunio91@hotmail.com',
+            'password' => Hash::make('fernando1991'),
+        ]);
+        Permission::create(['name' => 'webinar_editar','guard_name'=>'api']);
+        Permission::create(['name' => 'webinar_crear','guard_name'=>'api']);
+        Permission::create(['name' => 'webinar_eliminar','guard_name'=>'api']);
+        Permission::create(['name' => 'webinar_aprobar','guard_name'=>'api']);
+        // Roles
+        $admin = Role::create(['name' => 'admin','guard_name'=>'api']);
+        $organizador = Role::create(['name' => 'organizador','guard_name'=>'api']);
+        $usuario = Role::create(['name' => 'usuario','guard_name'=>'api']);
+
+        $admin->givePermissionTo([
+            'webinar_editar',
+            'webinar_crear',
+            'webinar_eliminar',
+            'webinar_aprobar',
+
+        ]);
+        $organizador->givePermissionTo([
+        'webinar_crear','webinar_editar'
+        ]);
+    }
+}
